85c640e6aa4f5f21d24d17f91c139e4da23df6e3
warley004 <warleyjunio2004@gmail.com>
2024-08-04 22:56:09 -0300

    feat: implementação dos arquivos "Jogo.hpp" e "Jogo.cpp"

---
+++ ProjetoFinal/include/Jogo.hpp
@@ -0,0 +1,14 @@
+#ifndef JOGO_HPP
+#define JOGO_HPP
+
+class Jogo {
+public:
+    virtual ~Jogo() = default;
+    virtual void iniciar() = 0;
+    virtual void imprimirTabuleiro() = 0;
+    virtual bool validarJogada(int linha, int coluna) = 0;
+    virtual bool verificarVitoria() = 0;
+    virtual void realizarJogada(int linha, int coluna) = 0;
+};
+
+#endif 
